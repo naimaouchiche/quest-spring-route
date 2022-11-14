9d9157f47dbc9f516af0e330966a80e2f6c5d837
FIRST_AOUCHICHE LAST_NAIM <naim.aouchiche@sncf.fr>
2022-11-14 15:50:49 +0100

    quete route

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,29 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public Doctor doctor(@PathVariable int number) {
 
-        return null;
+        if (number == 13) {
+
+            return new Doctor(13, "Jodie Whittaker");
+        } else if (number > 0 && number < 13) {
+
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, reason: "Error 303");
+        } else {
+
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, reason: "Impossible de récupérer l'incarnation " + number);
+        }
     }
 }
